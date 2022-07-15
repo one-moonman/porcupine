145a1259f886c590b5e35170afb148980eeb5a1b
Ni|cola Manoilov <nicola.manoilov@gmail.com>
2022-07-15 15:01:38 +0300

    initial commit of v2

--- api/routes.go
+++ api/routes.go
@@ -1,17 +1,1 @@
 package api
-
-import "github.com/gin-gonic/gin"
-
-func (s *Server) Routes() *gin.Engine {
-	router := s.router
-
-	v1 := router.Group("/api/v1")
-	{
-		v1.POST("/register", s.handlers.Register())
-		v1.POST("/login", s.handlers.Login(), s.handlers.GenerateTokens())
-		v1.POST("/refresh", s.handlers.VerifyRefreshToken(), s.handlers.GenerateTokens())
-		v1.POST("/logout", s.handlers.VerifyAccessToken(), s.handlers.Logout())
-		v1.GET("/me", s.handlers.VerifyAccessToken(), s.handlers.Me())
-	}
-	return router
-}
--- main.go
+++ main.go
@@ -1,12 +1,1 @@
 package main
-
-import (
-	"bug-free-octo-broccoli/api"
-	"bug-free-octo-broccoli/configs"
-)
-
-func main() {
-	configs.Init()
-	server := new(api.Server)
-	server.Run()
-}
